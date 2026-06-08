4f3cc134aabfd2cdecaf9b77ec7caf7b1323a1f2
pmiku <pmikulko2@gmail.com>
2026-06-08 14:21:19 +0300

    init commit

---
+++ src/test/java/com/testtask/krainet/KrainetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.krainet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrainetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
